0255215b49a629300bb45e9adc00563b7130fcb8
zelwake <michalhladik2@seznam.cz>
2022-07-20 18:15:14 +0200

    v2.3
    starting with jquery

--- v2/scripts.js
+++ v2/scripts.js
@@ -3,11 +3,17 @@ let todoNumber = 1
 
 $("#buttonAdd").click(function () {
     item = $("input").val();
-    $("ul").append($(`<li id="todo-${todoNumber}" class="listItem"> ${item}</li>`));
+    $("ul").append($(`
+                <li class="text">
+                    <span id="text_${todoNumber}" class="todoListItem">${item}</span>
+                    <img id="del_${todoNumber}" src="delete.svg" class="deleteButton" alt="">
+                    <img id="done_${todoNumber}" src="done.svg" class="doneButton" alt="">
+                </li>
+                `));
     todoNumber++;
 });
 
-$(`ul`).on("click", "li.listItem", function () {
+$(`ul.li`).on("click", "img.doneButton", function () {
     var id = $(this).attr("id");
     $("#" + id).css("text-decoration", "line-through")
 })
