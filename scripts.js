9e279d9a76f2ffe366ed9b678600df42da260e3a
zelwake <michalhladik2@seznam.cz>
2022-07-20 15:56:20 +0200

    working marking in first version

--- scripts.js
+++ scripts.js
@@ -1,21 +1,13 @@
 let item = ""
 let todoNumber = 1
 
-$("#buttonAdd").on( "click", function() {
+$("#buttonAdd").click(function () {
     item = $("input").val();
-    $("ul").append($(`<li id="todo-${todoNumber}"> ${item}</li>`));
+    $("ul").append($(`<li id="todo-${todoNumber}" class="listItem"> ${item}</li>`));
     todoNumber++;
+});
+
+$(`ul`).on("click", "li.listItem", function () {
+    var id = $(this).attr("id");
+    $("#" + id).css("text-decoration", "line-through")
 })
-
-/*works on first item only, crosses every single item that has been generated so far*/
-$('li').on("click", function() {
-    $('li').css("text-decoration", "line-through")
-})
-
-
-/* doesnt work at all
-$(`#todo-${todoNumber}`).on("click", "li", function() {
-    $(`#todo-${todoNumber}`).css("text-decoration", "line-through")
-})
-
-*/
